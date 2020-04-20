84328081b8c7ac0f4661df7e5f5dfff14a992c85
jp631 <47936264+jp631@users.noreply.github.com>
2020-04-20 16:42:11 -0400

    make link form more responsive

--- src/components/imageLinkForm/imageLinkForm.js
+++ src/components/imageLinkForm/imageLinkForm.js
@@ -9,9 +9,9 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
      </p>
      <div className="center">
         <div className="form center pa4 br3 shadow-5">
-        <input className="f4 pa2 w-70 center" placeholder="Enter an image link here!" type="text" onChange={onInputChange}/>
+        <input className="f4 pa2 center" placeholder="Enter an image link here!" type="text" onChange={onInputChange}/>
       <button
-        className="w-30 grow f4 link ph3 pv2 dib white bg-purple"
+        className="grow f4 link ph3 pv2 dib white bg-purple"
          onClick={onButtonSubmit} >
          Detect
        </button>
